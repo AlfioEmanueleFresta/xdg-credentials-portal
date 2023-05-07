6fc95fe32d0fceddd238a2af15b79738d9ca2032
Alfie Fresta <alfie.fresta@gmail.com>
2023-05-07 16:07:57 +0100

    fixed derive; added unit tests

--- libwebauthn/src/transport/cable/crypto.rs
+++ libwebauthn/src/transport/cable/crypto.rs
@@ -12,29 +12,13 @@ pub enum KeyPurpose {
     PSK = 3,
 }
 
-/*
-
-
-func derive(output, secret, salt []byte, purpose keyPurpose) {
-    if uint32(purpose) >= 0x100 {
-        panic("unsupported purpose")
-    }
-
-    var purpose32 [4]byte
-    purpose32[0] = byte(purpose)
-
-    h := hkdf.New(sha256.New, secret, salt, purpose32[:])
-    if n, err := h.Read(output); err != nil || n != len(output) {
-        panic("HKDF error")
-    }
-} */
 
 pub fn derive(secret: &[u8], salt: Option<&[u8]>, purpose: KeyPurpose) -> Vec<u8> {
     let mut purpose32 = [0u8; 4];
     purpose32[0] = purpose as u8;
 
     let hkdf = Hkdf::<Sha256>::new(salt, secret);
-    let mut output = vec![0u8; 32];
+    let mut output = vec![0u8; 64];
     hkdf.expand(&purpose32, &mut output).unwrap();
     output
 }
@@ -67,4 +51,29 @@ pub fn trial_decrypt_advert(eid_key: &[u8], candidate_advert: &[u8]) -> Option<V
     }
 
     Some(block.to_vec())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::derive;
+    use super::KeyPurpose;
+    
+    #[test]
+    fn derive_eidkey_nosalt() {
+        let input = hex::decode("00112233445566778899aabbccddeeff").unwrap();
+        let output = derive(&input, None, KeyPurpose::EIDKey);
+        let expected = hex::decode("efafab5b2c84a11c80e3ad0770353138b414a859ccd3afcc99e3d3250dba65084ede8e38e75432617c0ccae1ffe5d8143df0db0cd6d296f489419cd6411ee505").unwrap();
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn derive_eidkey_salt() {
+        let input = hex::decode("00112233445566778899aabbccddeeff").unwrap();
+        let salt = hex::decode("ffeeddccbbaa998877665544332211").unwrap();
+        let output = derive(&input, Some(&salt), KeyPurpose::EIDKey);
+        let expected = hex::decode("168cf3dd220a7907f8bac30f559be92a3b6d937fe5594beeaf1e50e35976b7d654dd550e22ae4c801b9d1cdbf0d2b1472daa1328661eb889acae3023b7ffa509").unwrap();
+        assert_eq!(output, expected);
+    }
+
+
 }
